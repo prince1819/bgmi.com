ecbe7a3962e3403c8ba67107c625abb2abad8106
prince1819 <115636704+prince1819@users.noreply.github.com>
2024-08-09 22:22:14 +0530

    watcher.py

--- watcher.py
+++ watcher.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = '7262217536:AAHX6ROfNRk1ZH9ZcwokcwvyR4jFtk3fKP8'
-ADMIN_ID = '1065510487'
+API_TOKEN = '7338021238:AAHCVrKdhDkzJptMQaXJquUyZ0Se47Pi8OI'
+ADMIN_ID = '1171433448'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
